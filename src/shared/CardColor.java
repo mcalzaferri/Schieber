3c9a1d153bf50528dc9ab6e82c049d936bb94d27
mcalzaferri <26376420+mcalzaferri@users.noreply.github.com>
2018-03-06 11:19:41 +0100

    Update CardColor.java

--- src/shared/CardColor.java
+++ src/shared/CardColor.java
@@ -1,5 +1,14 @@
 package shared;
 
 public enum CardColor {
-	EICHEL,ROSE,SCHILTE,SCHELLE
+	EICHEL(1),
+	ROSE(1),
+	SCHILTE(2),
+	SCHELLE(2);
+	
+	private final int TrumpfMultiplicator;
+	
+	public CardColor(int TrumpfMultiplicator){
+		this.TrumpfMultiplicator = TrumpfMultiplicator;	
+	}
 }
