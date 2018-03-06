02de18efeecedffd1e58086b302c0dc3765e65ba
mcalzaferri <26376420+mcalzaferri@users.noreply.github.com>
2018-03-06 11:28:26 +0100

    Update GameMode.java

--- src/shared/GameMode.java
+++ src/shared/GameMode.java
@@ -1,5 +1,14 @@
 package shared;
 
 public enum GameMode {
-	SCHIEBEN,TRUMPF,OBENABE,UNEUFE
+	SCHIEBEN(0),
+	TRUMPF(1),
+	OBENABE(3),
+	UNEUFE(3);
+	
+	private final int ScoreMultiplicator;
+	
+	public GameMode(int ScoreMultiplicator){
+		this.ScoreMultiplicator = ScoreMultiplicator;
+	}
 }
