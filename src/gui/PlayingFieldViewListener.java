bb8d70a8076ee5751478b4184fe1bd1a6af88db2
mstieger <mstieger@10.0.0.40>
2018-03-10 13:32:13 +0100

    Added mouse clicked method

--- src/gui/PlayingFieldViewListener.java
+++ src/gui/PlayingFieldViewListener.java
@@ -1,5 +1,8 @@
 package gui;
 
+import java.awt.event.MouseEvent;
+
 public interface PlayingFieldViewListener {
 	public void cardSelected(PlayingFieldViewEvent event);
+	public void mouseClicked(PlayingFieldViewEvent event);
 }
