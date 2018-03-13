dfbb16cebfd76686379ee92d3929d5ab82620e29
mcalzaferri <mcalzaferri@pcklz111.ntb.ch>
2018-03-13 14:59:33 +0100

    Updated handling of ClientModel in controller and view

--- src/client/AbstractClientView.java
+++ src/client/AbstractClientView.java
@@ -2,7 +2,18 @@ package client;
 
 import shared.*;
 
-public interface ClientViewInterface {
+public abstract class AbstractClientView {
+	protected CardList hand;
+	protected CardList deck;
+	protected Trumpf trump;
+	protected Score score;
+	
+	public AbstractClientView(CardList hand, CardList deck, Trumpf trump, Score score) {
+		this.hand = hand;
+		this.deck = deck;
+		this.trump = trump;
+		this.score = score;
+	}
 	
 	public abstract void addObserver(ViewObserver observer);
 	
