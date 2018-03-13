6b3e3d52f5f49ca5d57a343b73ebef52bdf6b327
mcalzaferri <mcalzaferri@pcinf056.ntb.ch>
2018-03-13 11:18:30 +0100

    Updated Client <-> GUI interface

--- src/client/ClientViewInterface.java
+++ src/client/ClientViewInterface.java
@@ -3,10 +3,14 @@ package client;
 import shared.*;
 
 public interface ClientViewInterface {
+	
 	public abstract void addObserver(ViewObserver observer);
-	public abstract ServerAdress getServerAdress();
+	
 	public abstract void moveCardToDeck(Player source,Card card);
 	public abstract void moveDeckToPlayer(Player player);
+
 	public abstract void changeView(ViewEnumeration view);
+	public abstract void updateView();
+	
 	public abstract void updateScore(Score score);
 }
--- src/client/ViewObserver.java
+++ src/client/ViewObserver.java
@@ -3,7 +3,8 @@ package client;
 import shared.*;
 
 public interface ViewObserver {
-	public abstract void btnConnectClick();
+	public abstract void btnConnectClick(ServerAddress serverAddress);
+	public abstract void btnRestartClick();
 	public abstract void btnDisconnectClick();
 	public abstract void btnTrumpfClick(Trumpf trumpf);
 	public abstract void btnCardClick(Card card);
