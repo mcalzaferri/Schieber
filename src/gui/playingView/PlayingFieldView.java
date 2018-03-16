6d108ead70f715f28aaa76b2b6a175f9ea415d49
mstieger <mstieger@192.168.1.46>
2018-03-16 10:42:15 +0100

    Class moved into new package

--- src/gui/playingView/PlayingFieldView.java
+++ src/gui/playingView/PlayingFieldView.java
@@ -1,12 +1,13 @@
-package gui;
+package gui.playingView;
 
 import java.util.ArrayList;
 
 import client.ViewEnumeration;
 import client.ViewObserver;
+import gui.AbstractView;
 
 public class PlayingFieldView extends AbstractView{
-
+	
 	public PlayingFieldView(ViewEnumeration viewType, ArrayList<ViewObserver> observers) {
 		super(viewType, observers);
 		// TODO Auto-generated constructor stub
