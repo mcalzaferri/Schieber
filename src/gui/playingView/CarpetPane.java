eef0bfe56c9d4c5597978069fafc6cae1114fb5a
mstieger <mstieger@192.168.1.46>
2018-03-18 15:50:54 +0100

    Class created
    Class cant be used to test the applications behavior, because it depends
    on some other classes which are not yet implemented

---
+++ src/gui/playingView/CarpetPane.java
@@ -0,0 +1,68 @@
+package gui.playingView;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import client.ViewObserver;
+import gui.Gui;
+import shared.Card;
+import shared.CardList;
+import shared.client.ClientModel;
+
+public class CarpetPane extends JPanel{
+	private ClientModel data;
+	private ArrayList<ViewObserver> observers;
+	
+	public CarpetPane(ClientModel data, ArrayList<ViewObserver> observers) {
+		super();
+		this.data = data;
+		this.observers = observers;
+		this.setBackground(Color.green);
+		this.setPreferredSize(new Dimension(500, 500));
+		this.update();
+	}
+	
+	public void update() {
+		repaint();
+
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		
+			try {
+				int index = 0;
+				BufferedImage img;
+				for(Card c : this.data.getDeck()) {				
+				img = Gui.pictureFactory.getPicture(c, new Dimension(50, 80));
+					switch(index) {
+					case 0:
+						g.drawImage(img, 225, 335, null);
+						break;
+					case 1:
+						g.drawImage(img, 300, 210, null);
+						break;
+					case 2:
+						g.drawImage(img, 225, 85, null);
+						break;
+					case 3:
+						g.drawImage(img, 150, 210, null);
+						break;
+					}
+					index++;
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+					
+	}
+}
