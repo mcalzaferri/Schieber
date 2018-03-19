68e8fcbaab57ce652efcc9051c0ea30aaebaab7a
Sandro Fürer <sandro.fuerer@gmail.com>
2018-03-19 07:24:24 +0100

    - Added game flow to gameLogic class - Messages are not up to date

--- src/server/GameLogic.java
+++ src/server/GameLogic.java
@@ -20,6 +20,55 @@ public class GameLogic {
 		deck = new Card[36];
 		// fill deck with cards
 	}
+	
+	/**
+	 * Starting the Game and handles the Server Communication.
+	 */
+	public void startGame(Player player){
+		try{
+			while(true){
+				System.out.println("Waiting for Message");
+				com.waitForMessage();
+				System.out.println("Message recieved");
+				
+				Message message = com.getMessage();
+				
+				if(message instanceof JoinGameMessage){
+
+					HandOutCardsMessage handOutCards = new HandOutCardsMessage();
+					ChooseGameModeMessage chooseGameMode = new ChooseGameModeMessage();
+					com.send(handOutCards, player);
+					com.send(chooseGameMode, player);
+					
+					System.out.println("Message recieved: JoinGameMessage");
+					System.out.println("Player has been added");
+				}
+				else if(message instanceof ChosenGameModeMessage){
+					
+					PickedGameModeMessage pickedGameMode = new PickedGameModeMessage();
+					YourTurnMessage yourTurn = new YourTurnMessage();
+
+					com.broadcast(pickedGameMode);					
+					com.send(yourTurn);
+					
+					System.out.println("Message recieved: ChosenGameModeMessage");
+					System.out.println("Gamemode has been set");
+				}
+				else if(message instanceof PlaceCardMessage){
+					
+					PlacedCardMessage placedCard = new PlacedCardMessage(((PlaceCardMessage) message).card);
+					YourTurnMessage yourTurn = new YourTurnMessage();
+					
+					com.broadcast(placedCard);					
+					com.send(yourTurn, player);
+					
+					System.out.println("Message recieved: PlaceCardMessage");
+					System.out.println("Card has been placed");
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 
 	/**
 	 * @param p
--- src/server/ServerCommunication.java
+++ src/server/ServerCommunication.java
@@ -9,6 +9,14 @@ public class ServerCommunication extends Communication {
 	public ServerCommunication() {
 		super();
 	}
+	
+	public void waitForMessage(){
+		
+	}
+	
+	public Message getMessage(){
+		return null;
+	}
 
 	public void send(Player player, Message msg) throws IOException {
 		super.send(player.getSocketAddress(), msg);
