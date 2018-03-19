4b363be2f9ece5233f477d55e35c5ec252309ef5
chalbeisen <37087132+chalbeisen@users.noreply.github.com>
2018-03-19 21:13:53 +0100

    Add files via upload

---
+++ src/gui/EndView.java
@@ -0,0 +1,74 @@
+package gui;
+import javax.swing.*;
+
+import client.ViewEnumeration;
+import client.ViewObserver;
+import shared.ServerAddress;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
+public class EndView extends AbstractView{
+
+	private JButton newRoundButton;
+	private JButton disconnectButton;
+	
+    private static final int width = 400;
+    private static final int height = 200;
+    private static final int top = 
+    Toolkit.getDefaultToolkit().getScreenSize().height/2-height/2;
+    private static final int left = 
+    Toolkit.getDefaultToolkit().getScreenSize().width/2-width/2;
+	
+	public EndView(ViewEnumeration viewType, ArrayList<ViewObserver> observers) {
+		super(viewType, observers);
+		setTitle("Spiel beendet");
+		
+		newRoundButton = new JButton("neue Runde");
+		disconnectButton = new JButton("Beenden");
+		JPanel endViewPanel = new JPanel();
+		endViewPanel.setLayout(new GridLayout(2,1));
+		
+		newRoundButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				for(ViewObserver observer: observers)
+				{
+					observer.btnRestartClick();
+				}
+			}
+		});
+		
+		disconnectButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				for(ViewObserver observer: observers)
+				{
+					observer.btnDisconnectClick();
+				}
+			}
+		});
+		
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent arg0) {
+				dispose();
+			}
+		});
+		
+		endViewPanel.add(newRoundButton);
+		endViewPanel.add(disconnectButton);
+		
+		add(endViewPanel);
+		setSize(width,height);
+		setLocation(left,top);
+
+	}
+	
+	public void update()
+	{
+		
+	}
+	
+}
