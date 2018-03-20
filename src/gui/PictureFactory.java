1595eb58d5f1ba3d77c4cd523a5a09ea5812fd6d
mstieger <mstieger@DESKTOP-3G6S1BK.ntb.ch>
2018-03-20 15:13:31 +0100

    Added methods and enum for misc pictures

--- src/gui/PictureFactory.java
+++ src/gui/PictureFactory.java
@@ -13,6 +13,9 @@ import shared.Card;
 import shared.Trump;
 
 public class PictureFactory {
+	public enum Pictures{
+		Carpet, BlackBoard;
+	}
 	public BufferedImage getPicture(Card card) throws IOException {
 		String path = "res\\cards\\" + card.getColor().toString() + card.getValue().getDefaultValue() + ".png";
 		return this.loadImage(path);
@@ -28,6 +31,14 @@ public class PictureFactory {
 	}
 	public BufferedImage getPicture(Trump trump, Dimension size) throws IOException {
 		return this.getScaledPicture(this.getPicture(trump), size);
+	}
+	
+	public BufferedImage getPicture(Pictures picture) throws IOException {
+		String path = "res\\" + picture.toString() + ".png";
+		return this.loadImage(path);
+	}
+	public BufferedImage getPicture(Pictures picture, Dimension size) throws IOException {
+		return this.getScaledPicture(this.getPicture(picture), size);
 	}
 	private BufferedImage getScaledPicture(BufferedImage original, Dimension size) {
 		//Draws original picture into new picture using graphics object
