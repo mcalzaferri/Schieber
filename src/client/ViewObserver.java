5605bd5bdd9f5aafb39f232486308b86fbedd3e9
mstieger <mstieger@DESKTOP-3G6S1BK.ntb.ch>
2018-03-20 16:25:55 +0100

    Added method which is called when user clicks close button

--- src/client/ViewObserver.java
+++ src/client/ViewObserver.java
@@ -8,4 +8,5 @@ public interface ViewObserver {
 	public abstract void btnDisconnectClick();
 	public abstract void btnTrumpClick(Trump trump);
 	public abstract void btnCardClick(Card card);
+	public abstract void btnCloseWindowClick(ViewEnumeration view);
 }
