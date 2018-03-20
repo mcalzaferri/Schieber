909250f50cd921c7d05c928cf25b60f4812981e0
mstieger <mstieger@DESKTOP-3G6S1BK.ntb.ch>
2018-03-20 15:13:03 +0100

    Draw image of blackboard

--- src/gui/playingView/BlackBoardPane.java
+++ src/gui/playingView/BlackBoardPane.java
@@ -6,11 +6,15 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GraphicsEnvironment;
 import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 import client.ViewObserver;
+import gui.Gui;
+import gui.PictureFactory.Pictures;
 import shared.Card;
 import shared.CardList;
 import shared.client.ClientModel;
@@ -38,6 +42,13 @@ public class BlackBoardPane extends JPanel{
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
+		try {
+			BufferedImage img = Gui.pictureFactory.getPicture(Pictures.BlackBoard, this.getPreferredSize());
+			g.drawImage(img, 0, 0, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		g.setColor(Color.white);
 		g.setFont(font);
 		g.drawString("Test", 30, 30);
