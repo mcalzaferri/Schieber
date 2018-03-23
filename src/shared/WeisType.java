742bdfa24fc8973ccc36cf6748f777b708ce0331
mcalzaferri <mcalzaferri@pcklz226.ntb.ch>
2018-03-23 18:00:37 +0100

    Added ID to WeisType

--- src/shared/WeisType.java
+++ src/shared/WeisType.java
@@ -1,29 +1,63 @@
 package shared;
 
 public enum WeisType {
-	STOECK(20),
-	DREIBLATT(20),
-	VIERBLATT(50),
-	FUENFBLATT(100),
-	VIERGLEICHE(100),
-	VIERNELL(150),
-	SECHSBLATT(150),
-	VIERBAUERN(200),
-	SIEBENBLATT(200),
-	ACHTBLATT(250),
-	NEUNBLATT(300);
+	//				ID,	score
+	STOECK(			1,	20),
+	DREIBLATT(		2,	20),
+	VIERBLATT(		3,	50),
+	FUENFBLATT(		4,	100),
+	VIERGLEICHE(	5,	100),
+	VIERNELL(		6,	150),
+	SECHSBLATT(		7,	150),
+	VIERBAUERN(		8,	200),
+	SIEBENBLATT(	9,	200),
+	ACHTBLATT(		10,	250),
+	NEUNBLATT(		11,	300);
 	
 	//Datenfelder
+	private final int id;
 	private final int score;
 	
 	//Konstruktoren
-	WeisType(int score){
+	WeisType(int id, int score){
+		this.id = id;
 		this.score = score;
 	}
 		
 	//Methoden
+	public static WeisType getWeisTypeById(int id) {
+		switch(id) {
+		case 1:
+			return STOECK;
+		case 2:
+			return DREIBLATT;
+		case 3:
+			return VIERBLATT;
+		case 4:
+			return FUENFBLATT;
+		case 5:
+			return VIERGLEICHE;
+		case 6:
+			return VIERNELL;
+		case 7:
+			return SECHSBLATT;
+		case 8:
+			return VIERBAUERN;
+		case 9:
+			return SIEBENBLATT;
+		case 10:
+			return ACHTBLATT;
+		case 11:
+			return NEUNBLATT;
+		}
+		return null;
+	}
 		
 	//Getter und Setter
+	public int getId() {
+		return id;
+	}
+	
 	public int getScore() {
 		return score;
 	}
