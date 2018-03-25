0dbc7c08b63baa64720442c8473a48cda5f1a1fa
lgassner <37084765+lgassner@users.noreply.github.com>
2018-03-25 11:09:09 +0200

    implemented 4 Gleiche und Folgen Weise
    all Weise available now

--- src/bot/WeisLogic.java
+++ src/bot/WeisLogic.java
@@ -1,8 +1,12 @@
 package bot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.print.attribute.IntegerSyntax;
 
 import shared.Card;
+import shared.CardColor;
 import shared.CardValue;
 import shared.Trump;
 import shared.Weis;
@@ -11,21 +15,90 @@ import shared.WeisType;
 public class WeisLogic {
 	
 	private ArrayList<Card> cardsInHand;
+	private int[] cardIds; // advantage: easily sortable
 	private Trump trump;
 	private ArrayList<Weis> weise;
 	
 	public WeisLogic(ArrayList<Card> cardsInHand, Trump trump) {
 		this.cardsInHand = cardsInHand;
+		cardIds = cardsToIds(cardsInHand);
+		Arrays.sort(cardIds);
 		this.trump = trump;
 		weise = new ArrayList<>();
 	}
 	
 	public ArrayList<Weis> getWeise(){
+		
+		// Stöck?
 		if(checkStoeck()) {weise.add(new Weis(WeisType.STOECK,new Card(trump.getTrumpfColor(),CardValue.OBER),trump));}
+		
+		// 4 Gliche?
+		for(CardValue cv : check4Gliche()) {
+			if(cv == CardValue.UNDER) {
+				weise.add(new Weis(WeisType.VIERBAUERN, new Card(CardColor.EICHEL,CardValue.UNDER),trump));
+			} else if(cv == CardValue.NEUN) {
+				weise.add(new Weis(WeisType.VIERNELL, new Card(CardColor.EICHEL,CardValue.NEUN),trump));
+			} else {
+				weise.add(new Weis(WeisType.VIERGLEICHE, new Card(CardColor.EICHEL,cv),trump));
+			}
+		}
+		
+		// Blattfolgen?
+		int noOfCards = 1;
+		boolean last = false;
+		for(int i = 1; i<10; i++) {
+			if(i==9) {
+				last = true;	// final round for checking
+			}
+			if(!last && (cardIds[i]-cardIds[i-1])==1) { //consecutive cards, suppress check of non-existent index
+				noOfCards++;
+			} else {
+				Card card;
+				card = Card.getCardById(cardIds[i-noOfCards]);
+				WeisType weisType;
+				switch(noOfCards) {
+				case 3: 
+					weisType = WeisType.DREIBLATT;
+					weise.add(new Weis(weisType, card, trump));
+					break;
+				case 4:
+					weisType = WeisType.VIERBLATT;
+					weise.add(new Weis(weisType, card, trump));
+					break;
+				case 5:
+					weisType = WeisType.FUENFBLATT;
+					weise.add(new Weis(weisType, card, trump));
+					break;
+				case 6:
+					weisType = WeisType.SECHSBLATT;
+					weise.add(new Weis(weisType, card, trump));
+					break;
+				case 7:
+					weisType = WeisType.SIEBENBLATT;
+					weise.add(new Weis(weisType, card, trump));
+					break;
+				case 8:
+					weisType = WeisType.ACHTBLATT;
+					weise.add(new Weis(weisType, card, trump));
+					break;
+				case 9:
+					weisType = WeisType.NEUNBLATT;
+					weise.add(new Weis(weisType, card, trump));
+					break;
+				default:
+				}
+				noOfCards = 1;
+			}
+		}
+		
 		return weise;
 		
 	}
 	
+	/**
+	 * Function returns if we have the Stöck
+	 * @return true if Stöck are available
+	 */
 	private boolean checkStoeck() {
 		int stoeckCards = 0;
 		for(Card c : cardsInHand) {
@@ -36,6 +109,37 @@ public class WeisLogic {
 			}
 		}
 		return(stoeckCards == 2);
+	}
+	
+	/**
+	 * Function returns all 4 Gliche on Hand
+	 * @return List of CardValue where we have all 4
+	 */
+	private ArrayList<CardValue> check4Gliche() {
+		int[] noOfCards = new int[9];
+		ArrayList<CardValue> vierGliche = new ArrayList<>();
+		for(int i = 0; i < 9; i++) {
+			noOfCards[cardsInHand.get(i).getId()%10]++;
+		}
+		for(int i = 3; i < 9; i++) { // ignore 6,7,8 Card values
+			if(noOfCards[i] == 4){
+				vierGliche.add(Card.getCardById(i).getValue());
+			}
+		}
+		return vierGliche;	
+	}
+	
+	/**
+	 * auxiliary function to convert Card list to Array of IDs
+	 * @param List of Card
+	 * @return Integer Array of IDs
+	 */
+	private int[] cardsToIds(ArrayList<Card> cards) {
+		int[] ids = new int[cards.size()];
+		for(int i=0; i<cards.size();i++) {
+			ids[i] = cards.get(i).getId();
+		}
+		return ids;
 	}
 
 }
--- src/botTest/WeisTest.java
+++ src/botTest/WeisTest.java
@@ -12,17 +12,45 @@ public class WeisTest {
 	
 	public static void main(String[] args) {
 		
+		// Test if stöck are correctly recognized
 		BotIntelligence bot = new IntelligenceRandom();
 		int[] hand = {10,11,12,13,14,15,16,17,18};
 		bot.setHand(hand); // give all EICHEL
 		bot.setTrump(Trump.EICHEL);
 		
 		ArrayList<Weis> weise = bot.getWeise();
+		System.out.println("Stöck:");
 		for(Weis w: weise) {
 			System.out.println(w.getType());
 		}
+		System.out.println();
 		
+		// Test if 4 gliche are correctly recognized
+		int[] hand2 = {10,20,30,40,15,25,35,45,46};
+		bot.setHand(hand2);
+		bot.setTrump(Trump.EICHEL);
 		
+		weise = bot.getWeise();
+		System.out.println("4 Gliche:");
+		for(Weis w: weise) {
+			System.out.println(w.getType());
+			System.out.println(w.getOriginCard().getValue());
+		}
+		System.out.println();
+		
+		// Test if Folgen are correctly recognized
+		int[] hand3 = {12,11,10,20,21,13,30,31,32};
+		bot.setHand(hand3);
+		bot.setTrump(Trump.ROSE);
+
+		weise = bot.getWeise();
+		System.out.println("Folgen:");
+		for(Weis w: weise) {
+			System.out.println(w.getType());
+			System.out.println(w.getOriginCard().getColor());
+			System.out.println(w.getOriginCard().getValue());
+		}
+		System.out.println();
 	}
 
 }
