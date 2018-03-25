57d9ef8cdae3014a04951436d02cccd2bcd2c143
Maurus Calzaferri <Maurus Calzaferri@192.168.1.107>
2018-03-25 23:06:47 +0200

    Added id for easier translation

--- src/shared/Trump.java
+++ src/shared/Trump.java
@@ -1,20 +1,22 @@
 package shared;
 
 public enum Trump {
-	SCHIEBEN(GameMode.SCHIEBEN,null),
-	EICHEL(GameMode.TRUMPF,CardColor.EICHEL),
-	ROSE(GameMode.TRUMPF,CardColor.ROSE),
-	SCHILTE(GameMode.TRUMPF,CardColor.SCHILTE),
-	SCHELLE(GameMode.TRUMPF,CardColor.SCHELLE),
-	OBENABE(GameMode.OBENABE,null),
-	UNEUFE(GameMode.UNEUFE,null);
+	SCHIEBEN(1, GameMode.SCHIEBEN,null),
+	EICHEL(2, GameMode.TRUMPF,CardColor.EICHEL),
+	ROSE(3, GameMode.TRUMPF,CardColor.ROSE),
+	SCHILTE(4, GameMode.TRUMPF,CardColor.SCHILTE),
+	SCHELLE(5, GameMode.TRUMPF,CardColor.SCHELLE),
+	OBENABE(6, GameMode.OBENABE,null),
+	UNEUFE(7, GameMode.UNEUFE,null);
 	
 	//Datenfelder
+	private final int id;
 	private final GameMode gameMode;
 	private final CardColor trumpColor;
 	
 	//Konstruktoren
-	Trump(GameMode gameMode, CardColor trumpColor){
+	Trump(int id, GameMode gameMode, CardColor trumpColor){
+		this.id = id;
 		this.gameMode = gameMode;
 		this.trumpColor = trumpColor;
 	}
@@ -28,7 +30,31 @@ public enum Trump {
 		}
 	}
 	
+	public static Trump getById(int id) {
+		switch(id) {
+		case 1:
+			return SCHIEBEN;
+		case 2:
+			return EICHEL;
+		case 3:
+			return ROSE;
+		case 4:
+			return SCHILTE;
+		case 5:
+			return SCHELLE;
+		case 6:
+			return OBENABE;
+		case 7:
+			return UNEUFE;
+		}
+		return null;
+	}
+	
 	//Getter und Setter
+	public int getId() {
+		return id;
+	}
+	
 	public GameMode getGameMode() {
 		return gameMode;
 	}
