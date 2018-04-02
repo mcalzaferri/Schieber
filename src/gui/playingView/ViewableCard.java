9f86ab0249d29b2a30561c6524f6df8ee62d0519
mstieger <mstieger@192.168.1.46>
2018-04-02 19:16:13 +0200

    To allow dynamic scaling, the image is now drawn onto the card instead of setting the image icon

--- src/gui/playingView/ViewableCard.java
+++ src/gui/playingView/ViewableCard.java
@@ -2,18 +2,15 @@ package gui.playingView;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-
 import client.ViewObserver;
 import gui.Gui;
 import shared.Card;
-import shared.CardList;
 
 public class ViewableCard extends JButton{
 	private Card card;
@@ -26,15 +23,11 @@ public class ViewableCard extends JButton{
 		this.observers = observers;		
 		this.initButton();
 	}
-	
+
 	private void initButton() {
 		this.setPreferredSize(minCardSize);
-		try {
-			this.setIcon(new ImageIcon(Gui.pictureFactory.getPicture(card, this.getPreferredSize())));
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		this.setMinimumSize(minCardSize);
+		
 		this.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -43,6 +36,17 @@ public class ViewableCard extends JButton{
 				}
 			}		
 		});
+	}
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		try {
+			BufferedImage img = Gui.pictureFactory.getPicture(card, this.getSize());
+			g.drawImage(img, 0, 0, null);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 	public boolean hasCard() {
 		if(this.card == null) {
