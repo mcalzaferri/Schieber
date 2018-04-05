7753b146b92e1f8aea52f96b45ddacc372a5a5ef
mcalzaferri <mcalzaferri@pcklz222.ntb.ch>
2018-04-05 19:59:21 +0200

    Added ClientModelView (Empty but whatever)

---
+++ src/client/test/ClientModelView.java
@@ -0,0 +1,10 @@
+package client.test;
+
+import shared.client.ClientModel;
+
+public class ClientModelView {
+	ClientModel model;
+	public ClientModelView(ClientModel model) {
+		this.model = model;
+	}
+}
