ce2b5b2d05a57af3bef98a4383978502083b9589
mstieger <mstieger@10.0.0.40>
2018-04-16 21:44:51 +0200

    Documented and cleaned up class

--- src/gui/playingView/BlackBoardPane.java
+++ src/gui/playingView/BlackBoardPane.java
@@ -1,9 +1,9 @@
 package gui.playingView;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -16,44 +16,75 @@ import gui.Gui;
 import gui.PictureFactory.Pictures;
 import shared.client.ClientModel;
 
+/**
+ * This class represents a blackboard and displays the score of all
+ * teams and the rounds current trump.
+ * 
+ * @author mstieger
+ *
+ */
 public class BlackBoardPane extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7805022564116303797L;
 	private ClientModel data;
 	private ArrayList<ViewObserver> observers;
 	private BlackBoardDrawer drawer;
-	public final Font font = new Font("MV Boli" ,Font.PLAIN, 28);
-	public final Dimension minSize = new Dimension(350, 500);
-	public final Rectangle minInnerBounds = new Rectangle(50, 50, minSize.width - 100, minSize.height -100);
+	private BufferedImage img;
+	
+	public static final Font font = new Font("MV Boli" ,Font.PLAIN, 28);
+	public static final Dimension minSize = new Dimension(350, 500);
+	public static final Dimension minTrumpSize = new Dimension(50,70);
+	public static final Rectangle minInnerBounds = new Rectangle(50, 50, minSize.width - 100, minSize.height -100);
 	
 	public BlackBoardPane(ClientModel data, ArrayList<ViewObserver> observers) {
 		super();
-		this.data = data;
-		this.observers = observers;
 		this.drawer = new BlackBoardDrawer();
-		this.setPreferredSize(minSize);
-		this.setMinimumSize(this.getPreferredSize());
+		try {
+			this.img = Gui.pictureFactory.getPicture(Pictures.BlackBoard);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(data == null) {
+			throw new IllegalArgumentException("Data can not be null");
+		}
+		this.data = data;
+		this.observers = observers;	
+		this.setMinimumSize(minSize);
 		this.update();
 	}
 	
+	/**
+	 * Updates the blackboard. This means that the scores and the current trump are
+	 * drawn anew.
+	 */
 	public void update() {
 		//TODO
 		repaint();
 	}
 	
+	Rectangle r = new Rectangle();
+	Dimension dim = new Dimension();
+	Font f = font;
 	@Override
 	public void paint(Graphics g) {
-		super.paint(g);
-		try {
-			Image img = Gui.pictureFactory.getPicture(Pictures.BlackBoard, this.getSize());
-			g.drawImage(img, 0, 0, null);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		double scale = this.getSize().getHeight()/minSize.getHeight();
-		this.drawer.setScale(scale);
-		this.drawer.setFont(this.font);
-		this.drawer.setBounds(minInnerBounds);
-		this.drawer.drawText(g, new String[]{"Schieber", "Score Team 1", "Score Team 2"});
-		this.drawer.drawTrump(g, data.getTrump());
+		r.setRect(scale*minInnerBounds.x, scale*minInnerBounds.y,
+				scale*minInnerBounds.width, scale*minInnerBounds.height);
+		dim.setSize(scale*minTrumpSize.width, scale*minTrumpSize.height);
+		
+		g.setColor(getBackground());
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(Gui.pictureFactory.getScaledPicture(img, this.getSize()), 0, 0, null);
+		
+		g.setColor(Color.WHITE);
+		g.setFont(new Font(font.getFontName(), font.getStyle(), (int)(scale*font.getSize())));
+		drawer.setBounds(r);
+		drawer.drawTitle(g, "Schieber");
+		//TODO get score
+		drawer.drawText(g, new String[]{"Score Team 1", "Score Team 2"});
+		drawer.drawTrump(g, data.getTrump(), dim);
 	}
 }
