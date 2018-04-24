d84b5c0b10e2cb9721467432ce4003f231574c56
mstieger <mstieger@DESKTOP-3G6S1BK.ntb.ch>
2018-04-24 14:36:36 +0200

    Added new method to create a buffered image from an image

--- src/gui/PictureFactory.java
+++ src/gui/PictureFactory.java
@@ -3,6 +3,7 @@ package gui;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
@@ -62,5 +63,23 @@ public class PictureFactory {
     	URL url = new File(path).toURI().toURL();
         return  ImageIO.read(url);
     }
+	public static BufferedImage toBufferedImage(Image img)
+	{
+	    if (img instanceof BufferedImage)
+	    {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
+	}
 	
 }
