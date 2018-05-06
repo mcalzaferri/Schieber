92bdcab0c28337714db0eab7a1839655585be265
mstieger <mstieger@192.168.1.46>
2018-05-06 18:45:47 +0200

    Tests concerning flickering hand

--- src/gui/GuiTest.java
+++ src/gui/GuiTest.java
@@ -3,15 +3,18 @@ package gui;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.InetSocketAddress;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import client.AbstractClientView;
 import client.ViewEnumeration;
+import client.ViewObserver;
 import gui.Gui.MessageType;
-import shared.CardList;
+import shared.Card;
+import shared.Seat;
+import shared.Trump;
 
-public class GuiTest extends JFrame{
+public class GuiTest extends JFrame implements ViewObserver{
 	private AbstractClientView gui;
 	private ClientModelTest data;
 
@@ -23,6 +26,7 @@ public class GuiTest extends JFrame{
 		super();
 		data = new ClientModelTest();
 		gui = new Gui(data);
+		gui.addObserver(this);
 		this.initFrame();
 	}
 
@@ -37,12 +41,6 @@ public class GuiTest extends JFrame{
 				gui.changeView(ViewEnumeration.valueOf(box.getSelectedItem().toString()));
 				gui.showDialog("View changed", MessageType.INFORMATION);
 				gui.showDialog("This is to test if the thread stops", MessageType.INFORMATION);
-				
-				CardList list = new CardList();
-				for(int i = 0; i < 12; i++) {
-					list.add(ClientModelTest.createRandomCard());
-				}
-				data.setHand(list);
 				gui.updateView(ViewEnumeration.PLAYVIEW);
 			}
 			
@@ -54,5 +52,64 @@ public class GuiTest extends JFrame{
 		this.pack();
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void btnConnectClick(InetSocketAddress serverAddress, String username) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void btnRestartClick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void btnDisconnectClick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void btnTrumpClick(Trump trump) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void btnCardClick(Card card) {
+		System.out.println(card.getColor().toString() + card.getValue());
+//		Card[] cards = data.getHand().toArray();
+//		cards[0] = ClientModelTest.createRandomCard();
+//		data.updateHand(cards);
+		data.getHand().remove(card);
+		data.getHand().add(ClientModelTest.createRandomCard());
+		gui.updateView(ViewEnumeration.PLAYVIEW);	
+	}
+
+	@Override
+	public void btnCloseWindowClick(ViewEnumeration view) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void btnWeisActionChosen(boolean allowBroadcast) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void btnJoinTableClick(Seat preferedSeat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void btnChangeStateClick() {
+		// TODO Auto-generated method stub
+		
 	}
 }
