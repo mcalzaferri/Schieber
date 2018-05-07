4cfd8af6a1ea93433dacfafd67f5e93f13929fd8
mstieger <mstieger@10.0.0.40>
2018-05-07 09:41:39 +0200

    Improved speed of painting
    This countermeasure could already increase the drawing speed of the
    buttons. However it can still be made better.

--- src/gui/playingView/HandPane.java
+++ src/gui/playingView/HandPane.java
@@ -1,10 +1,9 @@
 package gui.playingView;
 
+import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.ArrayList;
 import client.ViewObserver;
 import gui.ObservableView;
@@ -35,40 +34,78 @@ public class HandPane extends ObservableView implements ActionListener{
 	 * Initialize the class' graphics
 	 */
 	private void initializeComponents() {
-		this.setDoubleBuffered(true);
 		setOpaque(false);
 		this.setLayout(new HandLayout());
 		this.setMinimumSize(new Dimension(CarpetPane.minCarpetSize.width, ViewableCard.minCardSize.height));
 	}
 	
-	/**
-	 * Creates a graphical representation of the clients hand.
-	 * If the hand is null, all the existing components are just
-	 * being removed and repainted.
+	/**Creates a graphical representation of the clients hand.
+	 * In order to prevent the cards from flickering due to repeatedly
+	 * calling this method, the cards in the components array are compared
+	 * to the hand. Only if data has changed, the buttons will be repainted.
+	 * TODO Find a better solution if there is time, e.g. draw cards directly
+	 * without using buttons
 	 */
 	public void update() {
-		this.removeAll();	//Remove all cards from panel's components list
+		ViewableCard vc;
+		boolean changed = false;
 		
-		//Add cards to panel if hand is not null
-		if(data.getHand() != null) {
-			for(Card c : data.getHand()) {
-				ViewableCard vc = new ViewableCard(c);
-				vc.addActionListener(this);
-				if(data.getDeck().toArray().length > 0) {
-					//isAllowed method can handle null values!!!
-					vc.setEnabled(c.isAllowed(data.getHand().toArray(), data.getDeck().get(0), data.getTrump()));
+		//Check if cards have changed
+		if(data.getHand() == null || getComponentCount() != data.getHand().size()) {
+			//If array size differs, the hand has surely changed
+			changed = true;
+		}
+		else {
+			/*
+			 * TODO Compare hand to components if size is the same
+			 * During regular game this should not occur. When a card is
+			 * played, the hand becomes smaller automatically.
+			 */
+		}
+
+		if(changed) {
+			//Create new components from hand
+			this.removeAll();
+			
+			if(data.getHand() != null) {
+				for(Card c : data.getHand()) {
+					vc = new ViewableCard(c);
+					vc.addActionListener(this);
+					doEnableCard(vc);
+					add(vc);
 				}
-				else {
-					vc.setEnabled(c.isAllowed(data.getHand().toArray(), null, data.getTrump()));
+			}	
+			validate();	//Redo the layout
+			
+		}else {
+			//Just do enabling of cards
+			if(data.getHand() != null) {
+				for(Component c : getComponents()) {
+					if(c instanceof ViewableCard) {
+						vc = (ViewableCard) c;
+						doEnableCard(vc);
+					}
 				}
-				add(vc);
-			}
-		}	
-		
-		validate();	//Redo the layout
+			}	
+		}
+	
 		repaint();	//Let panel draw its components
 	}
 
+	private void doEnableCard(ViewableCard vc) {
+		if(data.getDeck().toArray().length > 0) {
+			//isAllowed method can handle null values!!!
+			vc.setEnabled(vc.getCard().isAllowed(data.getHand().toArray(), 
+					data.getDeck().get(0), 
+					data.getTrump()));
+		}
+		else {
+			vc.setEnabled(vc.getCard().isAllowed(data.getHand().toArray(), 
+					null, 
+					data.getTrump()));
+		}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		for(ViewObserver vo : observers) {
