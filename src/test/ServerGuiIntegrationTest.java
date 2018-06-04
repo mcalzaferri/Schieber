dea0db48f3514788ae6b37db8c66e1b8142e714b
Johannes Neyer <johannes.neyer@ntb.ch>
2018-06-04 22:40:25 +0200

    📈

--- src/test/ServerGuiIntegrationTest.java
+++ src/test/ServerGuiIntegrationTest.java
@@ -2,13 +2,15 @@ package test;
 
 import java.io.IOException;
 
+import bot.BotApplication;
 import client.ClientApplication;
 import server.ServerApp;
+import shared.Seat;
 
 public class ServerGuiIntegrationTest {
 	static final int serverListenPort = 65000;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// Start server
 		new Thread(() -> {
 			try {
@@ -21,9 +23,9 @@ public class ServerGuiIntegrationTest {
 		//Start Gui
 		ClientApplication.main(null);
 
-		// Start 3 bots
+		// Start Bots
 //		for (int i = 0; i < 3; i++) {
-//			BotApplication.start("localhost",  serverListenPort);
+			BotApplication.start("localhost",  serverListenPort, Seat.SEAT1);
 //		}
 	}
 }
